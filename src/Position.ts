47b210b2cceebc1a8b49dfc25b09653d6f1c80b4
Hana Aliyah Mufidah <hanaaliyah@Hanas-MacBook-Air.local>
2025-02-25 18:37:40 +0700

    update event after refactor contract

--- src/Position.ts
+++ src/Position.ts
@@ -1,21 +1,62 @@
 import { ethereum, Bytes, BigInt } from "@graphprotocol/graph-ts";
 import {
   Borrow as BorrowEvent,
+  Repay as RepayEvent,
   SupplyCollateral as SupplyCollateralEvent,
   UserPosition as UserPositionEvent,
   WithdrawCollateral as WithdrawCollateralEvent,
 } from "../generated/templates/Position/Position";
 import {
   Borrow,
+  Repay,
   SupplyCollateral,
   UserPosition,
   WithdrawCollateral,
-  Position,
 } from "../generated/schema";
+
+export function handleUserPosition(event: UserPositionEvent): void {
+  let id = event.transaction.hash.toHex() + "-" + event.logIndex.toString();
+  let entity = new UserPosition(id);
+
+  entity.lendingPool = event.params.lendingPool;
+  entity.caller = event.params.caller;
+  entity.onBehalf = event.params.onBehalf;
+  entity.baseCollateral = event.params.baseCollateral;
+  entity.effectiveCollateral = event.params.effectiveCollateral;
+  entity.borrowShares = event.params.borrowShares;
+  entity.leverage = event.params.leverage;
+  entity.liquidationPrice = event.params.liquidationPrice;
+  entity.health = event.params.health;
+  entity.ltv = event.params.ltv;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+  entity.save();
+}
 
 export function handleBorrow(event: BorrowEvent): void {
   let id = event.transaction.hash.toHex() + "-" + event.logIndex.toString();
   let entity = new Borrow(id);
+
+  entity.lendingPool = event.params.lendingPool;
+  entity.caller = event.params.caller;
+  entity.onBehalf = event.params.onBehalf;
+  entity.baseCollateral = event.params.baseCollateral;
+  entity.effectiveCollateral = event.params.effectiveCollateral;
+  entity.borrowShares = event.params.borrowShares;
+  entity.leverage = event.params.leverage;
+  entity.liquidationPrice = event.params.liquidationPrice;
+  entity.health = event.params.health;
+  entity.ltv = event.params.ltv;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+  entity.save();
+}
+
+export function handleRepay(event: RepayEvent): void {
+  let id = event.transaction.hash.toHex() + "-" + event.logIndex.toString();
+  let entity = new Repay(id);
 
   entity.lendingPool = event.params.lendingPool;
   entity.caller = event.params.caller;
@@ -51,46 +92,6 @@ export function handleSupplyCollateral(event: SupplyCollateralEvent): void {
   entity.blockTimestamp = event.block.timestamp;
   entity.transactionHash = event.transaction.hash;
   entity.save();
-}
-
-export function handleUserPosition(event: UserPositionEvent): void {
-  let id = event.transaction.hash.toHex() + "-" + event.logIndex.toString();
-  let entity = new UserPosition(id);
-
-  entity.lendingPool = event.params.lendingPool;
-  entity.caller = event.params.caller;
-  entity.onBehalf = event.params.onBehalf;
-  entity.baseCollateral = event.params.baseCollateral;
-  entity.effectiveCollateral = event.params.effectiveCollateral;
-  entity.borrowShares = event.params.borrowShares;
-  entity.leverage = event.params.leverage;
-  entity.liquidationPrice = event.params.liquidationPrice;
-  entity.health = event.params.health;
-  entity.ltv = event.params.ltv;
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-  entity.save();
-
-  let positionId = event.params.onBehalf.toHex();
-  let position = Position.load(positionId);
-  if (position == null) {
-    position = new Position(positionId);
-  }
-  position.lendingPool = event.params.lendingPool;
-  position.caller = event.params.caller;
-  position.onBehalf = event.params.onBehalf;
-  position.baseCollateral = event.params.baseCollateral;
-  position.effectiveCollateral = event.params.effectiveCollateral;
-  position.borrowShares = event.params.borrowShares;
-  position.leverage = event.params.leverage;
-  position.liquidationPrice = event.params.liquidationPrice;
-  position.health = event.params.health;
-  position.ltv = event.params.ltv;
-  position.blockNumber = event.block.number;
-  position.blockTimestamp = event.block.timestamp;
-  position.transactionHash = event.transaction.hash;
-  position.save();
 }
 
 export function handleWithdrawCollateral(event: WithdrawCollateralEvent): void {
--- src/position-factory.ts
+++ src/position-factory.ts
@@ -1,5 +1,6 @@
-import { PositionCreated as PositionCreatedEvent } from "../generated/PositionFactory/PositionFactory"
-import { PositionCreated } from "../generated/schema"
+import { PositionCreated as PositionCreatedEvent, PositionDeleted as PositionDeletedEvent } from "../generated/PositionFactory/PositionFactory"
+import { PositionCreated, PositionDeleted } from "../generated/schema"
+import { Position as PositionTemplate } from "../generated/templates";
 
 export function handlePositionCreated(event: PositionCreatedEvent): void {
   let entity = new PositionCreated(
@@ -7,8 +8,22 @@ export function handlePositionCreated(event: PositionCreatedEvent): void {
   )
   entity.positionAddress = event.params.positionAddress
   entity.lendingPool = event.params.lendingPool
-  entity.baseCollateral = event.params._baseCollateral
-  entity.leverage = event.params._leverage
+  entity.baseCollateral = event.params.baseCollateral
+  entity.leverage = event.params.leverage
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+  entity.save()
+  PositionTemplate.create(event.params.positionAddress)
+}
+
+export function handlePositionDeleted(event: PositionDeletedEvent): void {
+  let entity = new PositionDeleted(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.positionAddress = event.params.onBehalf
+  entity.lendingPool = event.params.lendingPool
+  entity.caller = event.params.caller
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
